4744449d114708f048c2f67e4f311e20e5432061
Ninad Sawant <ninad5jan@gmail.com>
2026-09-08 22:03:05 +0100

    Broaden OIDC trust to repo-wide match

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -47,9 +47,7 @@ data "aws_iam_policy_document" "github_actions_trust" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-        "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
-        "repo:${var.github_org}/${var.github_repo}:pull_request",
-        "repo:${var.github_org}/${var.github_repo}:environment:production",
+        "repo:${var.github_org}/${var.github_repo}:*",
       ]
     }
   }
